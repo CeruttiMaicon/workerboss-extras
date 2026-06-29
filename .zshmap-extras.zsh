b01f259f38afbf79a19fe0937f753a7e347ad38f
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2026-06-29 18:44:56 -0300

    feat(aliases): rename ZapSender aliases to MarketingHub aliases
    Renamed the ZapSenderPlugin and ZapSenderBack aliases to MarketingHubPlugin and MarketingHubBack respectively to better reflect the project's new naming convention while maintaining the same functionality. This change improves consistency and clarity of the workspace configuration.

--- .zshmap-extras.zsh
+++ .zshmap-extras.zsh
@@ -62,8 +62,8 @@ alias VolleyTrackFront="clone_repo ~/Projects/VoleiClub-Front git@github.com:Zor
 alias VolleyTrackDocs="clone_repo ~/Projects/Zoren-Software.github.io git@github.com:Zoren-Software/Zoren-Software.github.io.git && cd ~/Projects/Zoren-Software.github.io"
 alias LandingPageBack="clone_repo ~/Projects/LandingPage-BackEnd-VoleiClub git@github.com:Zoren-Software/LandingPage-BackEnd-VoleiClub.git && cd ~/Projects/LandingPage-BackEnd-VoleiClub"
 alias LandingPageFront="clone_repo ~/Projects/LandingPage-FrontEnd-VoleiClub git@github.com:Zoren-Software/LandingPage-FrontEnd-VoleiClub.git && cd ~/Projects/LandingPage-FrontEnd-VoleiClub"
-alias ZapSenderPlugin="clone_repo ~/Projects/zap-sender-plugin git@github.com:Zoren-Software/zap-sender-plugin.git && cd ~/Projects/zap-sender-plugin"
-alias ZapSenderBack="clone_repo ~/Projects/zap-sender-back git@github.com:Zoren-Software/zap-sender-back.git && cd ~/Projects/zap-sender-back"
+alias MarketingHubPlugin="clone_repo ~/Projects/zap-sender-plugin git@github.com:Zoren-Software/zap-sender-plugin.git && cd ~/Projects/zap-sender-plugin"
+alias MarketingHubBack="clone_repo ~/Projects/zap-sender-back git@github.com:Zoren-Software/zap-sender-back.git && cd ~/Projects/zap-sender-back"
 
 # Atalhos
 alias cl="clear"
